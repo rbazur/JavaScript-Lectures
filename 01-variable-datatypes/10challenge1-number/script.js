50b99ee1b27fc3717821267bc41a6c1088c9063a
rbazur <razur@udm.edu.ph>
2024-07-02 04:22:23 -0700

    01 Variables and data Types

--- 01-variable-datatypes/10challenge1-number/script.js
+++ 01-variable-datatypes/10challenge1-number/script.js
@@ -1,5 +1,10 @@
+const myString = "developer";
+
 //Solution 1
-
+// myNewString = myString.charAt(0).toUpperCase() + myString.substring(1);
 //Solution 2
-
+// myNewString = myString[0].toUpperCase() + myString.substring(1);
 //Solution 3
+myNewString = `${myString[0].toUpperCase()}${myString.slice(1)}`;
+// myNewString = myString.charAt(0).toUpperCase() + myString.slice(1);
+console.log(myNewString); // 'Developer'
--- 01-variable-datatypes/13challenge2-html/script.js
+++ 01-variable-datatypes/13challenge2-html/script.js
@@ -1,12 +1,30 @@
 //random for 1 to 100
+const x = Math.floor(Math.random() * 100 + 1);
 //random 1 to 50
+const y = Math.floor(Math.random() * 50 + 1);
 
 //sum
+const sum = x + y;
+const sumOutput = `${x} + ${y} = ${sum}`;
+console.log(sumOutput);
 
 //diff
 
+const diff = x - y;
+const differenceOutput = `${x} - ${y} = ${diff}`;
+console.log(differenceOutput);
+
 //product
+const prod = x * y;
+const productOutput = `${x} * ${y} = ${prod}`;
+console.log(productOutput);
 
 //quotient
+const quot = x / y;
+const quotientOutput = `${x} / ${y} = ${quot}`;
+console.log(quotientOutput);
 
 //remainder
+const rm = x % y;
+const rmOutput = `${x} % ${y} = ${rm}`;
+console.log(rmOutput);
