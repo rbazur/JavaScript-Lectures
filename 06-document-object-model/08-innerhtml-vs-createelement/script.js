d01b4266df08e5da32ccf6cd0f4e49698a64d5c9
rbazur <razur@udm.edu.ph>
2024-07-19 03:55:21 -0700

    DOM Lectures

--- 06-document-object-model/08-innerhtml-vs-createelement/script.js
+++ 06-document-object-model/08-innerhtml-vs-createelement/script.js
@@ -12,4 +12,22 @@ function createListItem(item) {
 
 createListItem("Tuyo");
 
-//Clean & Performant
+// Clean & Performant
+function createNewItem(item) {
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(item));
+
+  const button = document.createElement("button");
+  button.className = "remove-item btn-link text-red";
+
+  const icon = document.createElement("i");
+  icon.className = "fa-solid fa-xmark";
+
+  button.appendChild(icon);
+  li.appendChild(button);
+
+  document.querySelector(".items").appendChild(li);
+}
+
+createListItem("Eggs");
+createNewItem("Cheese");
